acc2ad817ed61cd7366168c9b4664bafd3d8ce6c
putu eka mulyana <putuekamulyana97@gmail.com>
2019-07-17 10:54:40 +0800

    post data

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors')
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require("passport");
 const users = require("./routes/api/users.js");
+const store = require("./data/db.json");
 const app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({
 	extended:false
 }));
@@ -16,6 +19,13 @@ mongoose
 app.use(passport.initialize());
 require('./config/passport')(passport);
 app.use("/api/users",users);
+
+app.get('/api/store', (req, res) => {
+	res.status(200).json({
+	  store
+	})
+  });
+
 const port = process.env.PORT || 5000;
 
 app.listen(port,()=> console.log(`Server up and running on port ${port} !`)); 
